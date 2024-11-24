504eff0e1180bb7a9751447084f6afef8df6931b
ron john <aaronsophia1114@gmail.com>
2024-11-24 13:05:35 +0800

    change relative to absolute import

--- aes.py
+++ aes.py
@@ -556,7 +556,7 @@ if __name__ == '__main__':
     if len(sys.argv) < 2:
         print('Usage: ./aes.py encrypt "key" "message"')
         print('Running tests...')
-        from tests import *
+        from .tests import *
         run()
     elif len(sys.argv) == 2 and sys.argv[1] == 'benchmark':
         benchmark()
